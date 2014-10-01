35788a89ca002ec3203553719a4e05a7ffd25086
Lauren Lavoie <llavoie@grameenfoundation.org>
2014-10-01 11:55:36 -0700

    add stubs for remaining EMR entities

---
+++ src/main/java/org/motechproject/simpleemr/domain/Provider.java
@@ -0,0 +1,14 @@
+package org.motechproject.simpleemr.domain;
+
+import org.motechproject.mds.annotations.Entity;
+import org.motechproject.mds.annotations.Field;
+
+import org.apache.commons.lang.ObjectUtils;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class Provider {
+
+
+}
